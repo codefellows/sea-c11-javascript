e263c63fb36faac0d95db335a3a1fed3da20ad2b
SwampGuzzler <lc07@uw.edu>
2014-04-09 20:47:41 -0700

    Homework #2
    Finalized HW

--- students/LucasCotner/xor.js
+++ students/LucasCotner/xor.js
@@ -12,7 +12,7 @@ var preferredName = function (firstName, lastName) {
     } else {
         name2 = false;
     }
-    var message = ((name1 === true && name2 === true) ? "false" : (name1 === false && name2 === false) ? "false" : (name1 === true && name2 === false) ? "Lucas" : (name1 === false && name2 === true) ? "Cotner" : "Error!");
+    var message = ((name1 === true && name2 === true) ? "false" : (name1 === false && name2 === false) ? "false" : (name1 === true && name2 === false) ? firstName : (name1 === false && name2 === true) ? lastName : "Error!");
 
     console.log(message);
     /*if (name1 === true && name2 === true) {
