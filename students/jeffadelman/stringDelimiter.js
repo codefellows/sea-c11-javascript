189ae47da55bb137f3ba017f59fdd5407242568a
Jeff Adelman <jeffadelman8@yahoo.com>
2014-04-10 14:03:29 -0700

    fixed function, removed assignment specs

--- students/jeffadelman/stringDelimiter.js
+++ students/jeffadelman/stringDelimiter.js
@@ -1,17 +1,14 @@
-// Write a function that takes in a string seperated by a seperator, and will return an array 
-// of strings in between the seperators.
- 
 var stringDelimiter = function(x, y) {
   var delimited = [];
   while(x.indexOf(y) !== -1) {
-    delimited.push(x.substr(0, x.indexOf(y)));
+    var str = (x.substr(0, x.indexOf(y)));
+    if (str) {
+      delimited.push(str);
+    }
     x = x.slice(x.indexOf(y)+1);
   }
-  delimited.push(x);
+  if (x){
+    delimited.push(x);
+  }
   return delimited;
 };
-
-var sampleInput = 'asdf$lskd1234$asdo';
- 
-stringDelimiter(sampleInput, '$');
-// -> ['asdf', 'lskd1234', 'asdo']
