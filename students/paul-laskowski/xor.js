dc870536f023933ea0dfe9158a7a2fc52074b1ad
Paul Laskowski <pelaskowski@gmail.com>
2014-04-10 14:21:06 -0700

    Removed extraneous text and code from original assignment. Refactored preferredName function to make it shorter. Added a commented out ternary version of preferredName.

--- students/paul-laskowski/xor.js
+++ students/paul-laskowski/xor.js
@@ -1,38 +1,18 @@
-// Using a ternary operator. Implement an exclusive OR function called `preferredName` that has the following interface:
- 
-var FirstName,
-    LastName;
- 
-preferredName(FirstName, LastName);
-// -> false
- 
-FirstName = 'Hank';
- 
-preferredName(FirstName, LastName);
-// -> 'Hank'
- 
-LastName = 'Yates';
- 
-preferredName(FirstName, LastName);
-// -> false
- 
-FirstName = '';
- 
-preferredName(FirstName, LastName);
-// -> 'Yates'
 
 
-function preferredName(first, last) {
-	if (first && last){
+//shorter if else approach
+function preferredName(first,last) {
+	if ((first && last) || (!first && !last)) {
 		return false;
-	} else if (first) {
-		return first;
-	} else if (last) {
-		return last;
 	} else {
-		return false;
+		return (first || last);
 	}
-	
 }
 
+
+// //ternary operator approach
+// function preferredName(first, last) {
+// 	((first && last) || (!first && !last)) ? return false : return (first || last);
+// 	return x
+// }
 
