1fd896da722aa9a78a1c72974b2fae19c3cc578f
Cody Stebbins <cody@stebbins.io>
2014-04-10 01:35:24 -0700

    Submitting my XOR function for assignment 2

---
+++ students/chelseaLura/xor.js
@@ -0,0 +1,28 @@
+var FirstName,
+	LastName;
+
+function preferredName (FirstName, LastName) {
+    return(((FirstName && LastName) || (!FirstName && !LastName))
+        ? false 
+        : FirstName || LastName);
+};
+
+preferredName(FirstName, LastName);
+ 
+FirstName = 'Hank';
+ 
+preferredName(FirstName, LastName);
+ 
+LastName = 'Yates';
+ 
+preferredName(FirstName, LastName);
+ 
+FirstName = '';
+ 
+preferredName(FirstName, LastName);
+
+
+
+
+
+
