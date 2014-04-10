26b74b714f6f12033d992ea7339cbea818259cd0
Jeff Adelman <jeffadelman8@yahoo.com>
2014-04-10 10:36:04 -0700

    Changed return method

--- students/jeffadelman/stringDelimiter.js
+++ students/jeffadelman/stringDelimiter.js
@@ -1,8 +1,6 @@
 // Write a function that takes in a string seperated by a seperator, and will return an array 
 // of strings in between the seperators.
  
-var sampleInput = 'asdf$lskd1234$asdo';
-
 var stringDelimiter = function(x, y) {
   var delimited = [];
   while(x.indexOf(y) !== -1) {
@@ -10,8 +8,10 @@ var stringDelimiter = function(x, y) {
     x = x.slice(x.indexOf(y)+1);
   }
   delimited.push(x);
-  console.log(delimited);
+  return delimited;
 };
+
+var sampleInput = 'asdf$lskd1234$asdo';
  
 stringDelimiter(sampleInput, '$');
 // -> ['asdf', 'lskd1234', 'asdo']
