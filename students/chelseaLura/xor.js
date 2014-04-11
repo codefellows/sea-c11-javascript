7e3368b3ab6fc0184ca19f61c24cb421112aec43
ChelseaLura <mail@chelsealura.com>
2014-04-11 00:35:48 -0700

    Added comment

--- students/chelseaLura/xor.js
+++ students/chelseaLura/xor.js
@@ -1,5 +1,7 @@
 var FirstName,
 	LastName;
+	
+//Returns the given name if only one of the two names actually has content.
 
 function preferredName (FirstName, LastName) {
     return(((FirstName && LastName) || (!FirstName && !LastName))
