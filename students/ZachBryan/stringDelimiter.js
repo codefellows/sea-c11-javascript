10baf4799f5f5f74a746febb9dd02f8abd041c23
Zach Bryan <zachx1@gmail.com>
2014-04-12 13:25:01 -0700

    removed extra line info from function ec 1

--- students/ZachBryan/stringDelimiter.js
+++ students/ZachBryan/stringDelimiter.js
@@ -1,8 +1,3 @@
-// Write a function that takes in a string seperated by a seperator, and will return an array 
-// of strings in between the seperators.
-
-var sampleInput = 'asdf$lskd1234$asdo';
-
 var stringDelemiter = function(input, breakPoint){
 	var nList = [];
 	var limit, temp;
@@ -17,5 +12,3 @@ var stringDelemiter = function(input, breakPoint){
 	}
 	return(nList);
 } ;
-stringDelemiter(sampleInput, '$');
-// -> ['asdf', 'lskd1234', 'asdo']
