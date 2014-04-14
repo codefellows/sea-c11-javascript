61790ad5440e45457d11c71947667e9695ce688d
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-14 00:24:44 -0700

    fixed XOR with ^ function. Removed the other two.

--- students/yuliyaz/xor.js
+++ students/yuliyaz/xor.js
@@ -1,44 +1,12 @@
 //XOR with ^
 
- var FirstName,
+var FirstName,
     LastName;
-FirstName = 'h';
+FirstName = 'Hank';
  LastName = '';
  function preferredName (){
 var Name = FirstName + LastName;
-var FirstOrLast = ((FirstName && !LastName)^(LastName && !FirstName)) ? Name : false;
+var FirstOrLast = !FirstName ^ !LastName ? Name : false;
 return FirstOrLast;
  }
 
-
-//XOR with || and &&
-
- var FirstName,
-    LastName;
-FirstName = 'h';
- LastName = 'y';
- function preferredName (){
-var Name = FirstName + LastName;
-var FirstOrLast = ((FirstName || LastName) && !(FirstName && LastName)) ? Name : false;
-return FirstOrLast;
- }
-
-//if else
-
-  var FirstName,
-      LastName;
-FirstName = '';
- LastName = '';
- function preferredName (){
- var Name;
-if(FirstName && !LastName){
-	Name = FirstName;
-}
-else if(!FirstName && LastName ){
-	Name = LastName;
-}
-else {
-	Name = false;
-}
-return Name;
- }
