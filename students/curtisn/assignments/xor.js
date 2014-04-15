4858927293854b4126d2c9282616313ce9d8baf6
Currtis <currtis@me.com>
2014-04-14 20:12:22 -0700

    2nd interation for method

--- students/curtisn/assignments/xor.js
+++ students/curtisn/assignments/xor.js
@@ -1,42 +1,19 @@
 // Implement an exclusive OR function called `preferredName` that has the following interface:
- 
-// var FirstName,
-//     LastName;
- 
-// preferredName(FirstName, LastName);
-// // -> false
- 
-// FirstName = 'Hank';
- 
-// preferredName(FirstName, LastName);
-// // -> 'Hank'
- 
-// LastName = 'Yates';
- 
-// preferredName(FirstName, LastName);
-// // -> false
- 
-// FirstName = '';
- 
-// preferredName(FirstName, LastName);
-// -> 'Yates'
-
-
 
 function getPreferredName( fname, lname ) {
-	var FirstName,
-		LastName;
-
-	FirstName = fname || undefined;
-	LastName = lname || undefined;
-
-	if ( FirstName && LastName === undefined )
+	
+	if ( fname && lname )
 		return false;
 
-	else if ( LastName )
-		return FirstName + " " + LastName;
+	if ( !fname && !lname )
+		return false;
+
+	else if ( lname )
+		return lname;
 
 	else
-		return FirstName;
+		return fname;
 
 }
+
+// return (fname && lname ) ? false : fname || lname;
