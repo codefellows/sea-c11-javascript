6cd3c68d1857c75941f2b8d9f0c1c7be83b0f808
Billy Perry <bperry_1@hotmail.com>
2014-04-15 17:39:22 -0700

    added comments explaining what each function and variable declaration is doing

--- students/williamperry/OldestLivingFather.js
+++ students/williamperry/OldestLivingFather.js
@@ -36,8 +36,12 @@ var people = [
  
 
 function oldestLivingFather() {
+    
+    //initiating variables for use in below functions
     var oldestAge = 0;
     var oldestName = '';
+
+    //finding persons with father name (as "fatherNames")
     var fatherNames = _.pluck(people, 'father');
     
     _.each(people, function(person) {
@@ -47,20 +51,24 @@ function oldestLivingFather() {
             }
         } 
     });
-    
+
+    //determining name with oldest age
     _.each(people, function(person) {
         if (person.age === oldestAge){
         oldestName=person.name;
         }
     });
-    
+
+    //finding "living fathers" from people by eliminating those which do not have "age" property    
     var livingFathers = _.filter(people, function (person) {
         if (!person.hasOwnProperty('name') || !person.hasOwnProperty('age')) return false;
         
         return _.contains(fatherNames, person.name);
         
     });
-    
+
+
+    //initiating variable for use in _.each function below (churns through until it has found highest "age")
     var oldestLivingFather = {
         name: false,
         age: 0
@@ -69,7 +77,6 @@ function oldestLivingFather() {
     _.each(livingFathers, function (livingFather) {
         if (livingFather.age > oldestLivingFather.age) oldestLivingFather = livingFather 
     });
-    
     
 }
 oldestLivingFather();
