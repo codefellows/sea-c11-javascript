80fb9afb94264fd9e1f87db11f765988b3090012
Chelsea Lura <mail@chelsealura.com>
2014-04-15 17:31:16 -0700

    Changed variable & function names to be more precise and legible. Changed .substring implementation to .charAt.

--- students/chelseaLura/stringDelimiter.js
+++ students/chelseaLura/stringDelimiter.js
@@ -1,23 +1,23 @@
 function stringDelimiter(input, seperator) {
-    var delimiter = [];
+    var currentCharacters = [];
     var finalStrings = [];
     for (i = 0; i < input.length; i++) {
-        var currentLetter = input.substring(i, i + 1);
+        var currentLetter = input.charAt(i);
         if (currentLetter === seperator) {
-          concatDelimiter(delimiter, finalStrings);
-          delimiter = [];
+          pushJoinedCharacters(currentCharacters, finalStrings);
+          currentCharacters = [];
         } else {
-            delimiter.push(currentLetter);
+            currentCharacters.push(currentLetter);
         }
     }
-    concatDelimiter(delimiter, finalStrings);
+    pushJoinedCharacters(currentCharacters, finalStrings);
     return finalStrings;
 }
 
-function concatDelimiter (smallArray, largeArray) {
-    if(smallArray.length > 0) {
-      var string = smallArray.join('');
-      largeArray.push(string);
+function joinCharacters(characters, strings) {
+    if(characters.length > 0) {
+      var string = characters.join('');
+      strings.push(string);
     }
     
 }
