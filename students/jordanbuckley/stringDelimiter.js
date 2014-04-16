d8d43fb5e4148eda780b6b3a4465693009434848
jbkly <jbkly@users.noreply.github.com>
2014-04-16 00:19:56 -0700

    Delete stringDelimiter.js

--- students/jordanbuckley/stringDelimiter.js
+++
@@ -1,6 +0,0 @@
-// Write a function that takes in a string separated by a separator,
-// and will return an array of strings in between the separators.
-
-function stringDelimiter(inputString, separator) {
-	return inputString.split(separator);
-}
