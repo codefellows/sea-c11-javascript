d7b337c2a0ff0c9495fd12397c2fad246fe8ac20
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-16 21:00:54 -0700

    editted the function getFathersName to remove duplicates and undefined. Create forEach loop to return a name of a person who is in the getFathersName array and person's age is equal to highest age. Problem the hightest age currently is of the person who is not a father so my forEach returns undefined. I think i have to nest my oldest person function into the forEach loop.

--- students/yuliyaz/oldestLivingFather.js
+++ students/yuliyaz/oldestLivingFather.js
@@ -9,17 +9,34 @@ var highestAge = 0
 		}
 
 		return highestAge;
-		}
+		};
 
 
 var fathers = []
 function getFathersNames() {
 	for (var i = 0; i < people.length; i++) {
-		var fatherName = people[i].father
-	fathers.push(fatherName);
+		if (people[i].father){
+			var fatherName = people[i].father;
+			if (fathers.indexOf(fatherName) === -1) {
+				fathers.push(fatherName);
+			}
+		}
+	}
+	return fathers;
+};
+
+
+people.forEach(function(person){
+	var personName = person['name']
+	var fatherArray = getFathersNames()
+	var oldestAge = oldestPerson()
+	if (fatherArray.indexOf(personName) > -1 && person['age'] === oldestAge){
+		var oldestFatherName = person['name'];
 }
-return fathers;
-}
+return oldestFatherName
+});
+
+
 
 
 
