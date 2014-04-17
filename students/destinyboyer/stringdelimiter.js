6311bad2ea5f6bb512c7ec2b3b4f4897dfae5a5b
Destiny Boyer <destinyboyer92@gmail.com>
2014-04-16 19:06:47 -0700

    second commit for stringdelimiter, using index of and a do/while loop

--- students/destinyboyer/stringdelimiter.js
+++ students/destinyboyer/stringdelimiter.js
@@ -1,7 +1,27 @@
-var sampleInput;
-var separator = "$"
+var sampleInput = 'asdf$1skd1234$asdo';
+var delimiter = '$';
 
-function stringDelimiter (sampleInput, separator) {
-	var splitString = sampleInput.split("$");
-	return splitString;
+function stringDelimiter (sampleInput, delimiter) {
+
+var delPosition;
+var subString;
+var stringtoArray = [];
+var initialPostion = 0;
+
+do {
+
+delPosition = sampleInput.indexOf(delimiter);
+
+if (delPosition === -1){
+
+delPosition = sampleInput.length;
+}
+
+subString = sampleInput.substring(initialPostion, delPosition);
+stringtoArray.push(subString);
+sampleInput = sampleInput.substring(delPosition +1);
+}
+
+while (sampleInput);
+return stringtoArray;
 }
