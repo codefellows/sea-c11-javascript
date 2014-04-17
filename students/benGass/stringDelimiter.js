c7fea99c5f6c4b2dbddeb79848add7f675705a39
bgas <benjamin12g@gmail.com>
2014-04-16 20:21:41 -0700

    stringDelimiter more concise and readable

--- students/benGass/stringDelimiter.js
+++ students/benGass/stringDelimiter.js
@@ -1,15 +1,16 @@
-var stringDelemiter = function(strIn, sChar){
+var stringDelimiter = function(strIn, sChar){
 // (string, special character) -> array of strings seperated but not including special character
   var arrayOut =[]; 
   while (strIn){
     // index of special char
     var sCharIndex = strIn.indexOf(sChar);
-    var charPresent = (sCharIndex ==-1) ? strIn.length : sCharIndex;
+    if (sCharIndex == -1) { sCharIndex = strIn.length; }
     // str to be pushed to arrayOut
-    strPart = strIn.substr(0, charPresent);
+    strPart = strIn.substr(0, sCharIndex);
     arrayOut.push(strPart);
     // used parts of input string removed
-    strIn = strIn.substr(((charPresent==strIn.length)? strIn.length: charPresent+1), strIn.length); 
+    strIn = strIn.substr(sCharIndex+1, strIn.length); 
   };
   return arrayOut;
 };
+
