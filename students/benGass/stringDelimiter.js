d066078c11679d2f56b82de5a84f726473806a56
bgas <benjamin12g@gmail.com>
2014-04-16 19:08:05 -0700

    stringDelim with charPres var broken out

--- students/benGass/stringDelimiter.js
+++ students/benGass/stringDelimiter.js
@@ -1,15 +1,15 @@
 var stringDelemiter = function(strIn, sChar){
 // (string, special character) -> array of strings seperated but not including special character
   var arrayOut =[]; 
-  while (strIn.length != 0){
+  while (strIn){
     // index of special char
-    var sCharI = strIn.indexOf(sChar);
+    var sCharIndex = strIn.indexOf(sChar);
+    var charPresent = (sCharIndex ==-1) ? strIn.length : sCharIndex;
     // str to be pushed to arrayOut
-    strPart = strIn.substr(0, (sCharI !=-1) ? sCharI : strIn.length);
+    strPart = strIn.substr(0, charPresent);
     arrayOut.push(strPart);
     // used parts of input string removed
-    strIn = strIn.substr((sCharI !=-1) ? sCharI+1 : strIn.length, strIn.length);   
+    strIn = strIn.substr(((charPresent==strIn.length)? strIn.length: charPresent+1), strIn.length); 
   };
   return arrayOut;
 };
-
