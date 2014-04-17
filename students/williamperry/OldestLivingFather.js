9a1e6f51b0191f2e6c37ea67863afb573fc41f95
Billy Perry <bperry_1@hotmail.com>
2014-04-17 08:41:27 -0700

    my Oldest Living Father script, that actually works now

--- students/williamperry/OldestLivingFather.js
+++ students/williamperry/OldestLivingFather.js
@@ -1,82 +1,27 @@
-var people = [
-  {
-    name: 'Hank',
-    age: 29,
-    father: 'Don'
-  },
-  {
-    name: 'Deborah',
-    age: 30,
-    father: 'Don'
-  },
-  {
-    name: 'Danny',
-    age: 24,
-    father: 'Don'
-  },
-  {
-    name: 'Don',
-    age: 64,
-    father: 'Haskell'
-  },
-  {
-    name: 'Haskell',
-    father: 'Finus'
-  },
-  {
-    name: 'Finus'
-  },
-  {
-    name: 'Jeff',
-    age: 75,
-    father: 'Johnny'
-  }
-]
 
- 
+    function oldestLivingFather(people) {
 
-function oldestLivingFather() {
-    
-    //initiating variables for use in below functions
-    var oldestAge = 0;
-    var oldestName = '';
-
-    //finding persons with father name (as "fatherNames")
-    var fatherNames = _.pluck(people, 'father');
-    
-    _.each(people, function(person) {
-        if (person.age) {
-            if(person.age > oldestAge) {
-              oldestAge = person.age;
-            }
-        } 
-    });
-
-    //determining name with oldest age
-    _.each(people, function(person) {
-        if (person.age === oldestAge){
-        oldestName=person.name;
-        }
-    });
-
-    //finding "living fathers" from people by eliminating those which do not have "age" property    
-    var livingFathers = _.filter(people, function (person) {
-        if (!person.hasOwnProperty('name') || !person.hasOwnProperty('age')) return false;
         
-        return _.contains(fatherNames, person.name);
+        var fatherNames = _.pluck(people, 'father');
+        fatherNames = _.uniq(fatherNames);
+        fatherNames = _.compact(fatherNames);
+
+        var Fathers = [];
         
-    });
-
-
-    //initiating variable for use in _.each function below (churns through until it has found highest "age")
-    var oldestLivingFather = {
-        name: false,
-        age: 0
-    };
-    
-    _.each(livingFathers, function (livingFather) {
-        if (livingFather.age > oldestLivingFather.age) oldestLivingFather = livingFather 
-    });
-    
-}
-oldestLivingFather();
+        _.each(fatherNames, function (fatherName) {
+            _.each(people, function (person) {
+                if (fatherName === person.name) {
+                 Fathers.push(person);   
+                }
+            });
+        });
+        
+        var oldestFather;
+        
+       oldestFather = _.max(Fathers, function(Father) { 
+           return Father.age; });
+        
+        return oldestFather.name;
+        
+    }
+var oldGuy = oldestLivingFather(people);
