6dc66d06588db66bd1fec1c153c3502ec9276a77
Destiny Boyer <destinyboyer92@gmail.com>
2014-04-16 20:11:56 -0700

    streamline xor.js using bang bang and the correct number of equal signs

--- students/destinyboyer/xor.js
+++ students/destinyboyer/xor.js
@@ -1,20 +1,9 @@
 var firstName,
 	lastName;
 
-	//defining initial variables
 
 function isEmptyString(stringtoCheck){
-	if (stringtoCheck == undefined) {
-		return false;
-
-		//if a person enters nothing it would show up as false
-
-	} else if (stringtoCheck.length = 0) {
-		return false;
-
-		//ensures that even if the person enters "" it won't appear as a name
-
-	} else {
+	if (!!stringtoCheck){
 		return true;
 	}
 }
@@ -27,6 +16,3 @@ function preferredName(firstName, lastName) {
 		return firstName;
 	}
 }
-
-//preferredName function has a xor as the first conditional as there are two cases in which the code should return false.
-//if the code passes the xor statement then we can use less complex conditionals
