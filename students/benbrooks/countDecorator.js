8e09bafe99a10e8c43f08ecfe5ca1de9163e3cdc
Ben Brooks <ben@benbrooks.net>
2014-04-18 15:26:41 -0700

    Removing the modified assignment details

--- students/benbrooks/countDecorator.js
+++ students/benbrooks/countDecorator.js
@@ -7,6 +7,7 @@ function countDecorator(fn) {
 
         // Return original function value
         return fn.apply(null, arguments);
+
     }
 
     // Set initial call count
@@ -14,23 +15,3 @@ function countDecorator(fn) {
     return countInjector;
 
 }
-
-// Implement a decorator function that takes 
-// a function as an argument and will track
-// how many times the passed function was called.
-
-function Add(x, y) {
-    return x + y;
-}
-
-var addAndCountCalls = countDecorator(Add);
-
-console.log(addAndCountCalls(1, 1));
-// -> 2 Dont pay attention to this number. The important part is we called the `Add` function once.
-console.log(addAndCountCalls.callCount);
-// -> 1
-
-console.log(addAndCountCalls(2, 2));
-// -> 4 Dont pay attention to this number either. The important part is we called the `Add` function again.
-console.log(addAndCountCalls.callCount);
-// -> 2
