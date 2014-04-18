1659426265702332b4f1e92a06a7fe91b5790f7b
SwampGuzzler <lc07@uw.edu>
2014-04-18 09:23:51 -0700

    OldestLivingFather Finalized

--- students/LucasCotner/OldestLivingFather.js
+++ students/LucasCotner/OldestLivingFather.js
@@ -1,27 +1,13 @@
 var oldestLivingFather = function(people) { 
-    var fathers = _.pluck(people, 'father');   
-    var livingPeople = _.filter(people, 'age');   
-    var livingPeople2 = _.pluck(livingPeople, 'name');
-    var oldestFather;
-    var oldestAge = _.reduce( people, function( age, person ) {
-        var leng = people.length - 1;
-        if ( age > person.age ) {
-            if (person.name === people[leng].name) {
-                return oldestFather;
-            }
-            return age;
-        } else if ((_.contains(fathers,person.name)) && (_.contains(livingPeople2,person.name))) {    
-            age = person.age;
-            oldestFather = person.name;
-            if (person.name === people[leng].name) {
-                return oldestFather;
-            }
-            return age;            
-        } else {
-            if (person.name === people[leng].name) {     
-                return oldestFather;
-            }
-            return age;
-        }
+    var fathers = _.compact(_.unique(_.pluck(people, 'father')));
+
+    var matchingFathers = []; 
+    fathers.forEach(function(name) {
+        matchingFathers.push(_.reduce(_.where(people, {name: name})));
     });
+    
+    var oldestLivingFather = _.max(_.compact(matchingFathers), function(dad) {
+      return dad.age
+    });
+    return oldestLivingFather.name;
 };
