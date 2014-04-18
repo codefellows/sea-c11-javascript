9f3748f2e69d62c10d251209eb59d9bc7835a306
Jeff Adelman <jeffadelman8@yahoo.com>
2014-04-17 22:17:04 -0700

    Final plain JavaScript version

--- students/jeffadelman/oldestLivingFather.js
+++ students/jeffadelman/oldestLivingFather.js
@@ -1,12 +1,12 @@
 var oldestLivingFather = function(people){
   var fathersArray = [],
       livingPeople = [],
-      oldestLivingFather = 'There are no living fathers';
+      oldestLivingFather = false;
 
   //Find all the fathers and push their names to the fathersArray
   for(i=0; i< people.length; i++){
     var father = people[i].father;
-    if (father !== undefined){
+    if (father){
       if (fathersArray.indexOf(father) == -1){
       fathersArray.push(father);
       }
@@ -15,26 +15,27 @@ var oldestLivingFather = function(people){
 
   //Find all the living people and push them to the livingPeople array
   for(i=0; i<people.length; i++){
-    if(people[i].age !== undefined){
-      livingPeople.push(people[i]);
+    var person = people[i];
+    if(person.age){
+      livingPeople.push(person);
     }
   }
 
   //Sort the livingPeople array by age
   livingPeople.sort(function(a,b) { 
-    return parseInt(b.age) - parseInt(a.age); 
+    return ~~b.age - ~~a.age; 
   });
 
   //Look through the livingPeople array to find the first person who is also in the fathersArray.
-  //If there are no living fathers return "There are no living fathers."
+  //If there are no living fathers return "false"
   for (i=0; i< livingPeople.length; i++){
     var name = livingPeople[i].name;
-    if(fathersArray.indexOf(name) !== -1){
+    if(fathersArray.indexOf(name) > -1){
       oldestLivingFather = name;
       break;
     }
   }
-
+  
   return oldestLivingFather;
 };
 
