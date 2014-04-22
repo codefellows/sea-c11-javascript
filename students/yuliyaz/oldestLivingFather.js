d8c223180e98198562788b259808900267ce4a17
Yuliya Zakharenko <rockroada@gmail.com>
2014-04-21 19:02:35 -0700

    wrapping everything in a function

--- students/yuliyaz/oldestLivingFather.js
+++ students/yuliyaz/oldestLivingFather.js
@@ -1,34 +1,28 @@
-
-
-var fathers = []
-function getFathersNames() {
-	for (var i = 0; i < people.length; i++) {
-		if (people[i].father){
-			var fatherName = people[i].father;
-			if (fathers.indexOf(fatherName) === -1) {
-				fathers.push(fatherName);
-			}
-		}
-	}
-	return fathers;
-};
-
-var highestAge = 0
-var oldestFather
+var highestAge = 0;
+var oldestFather;
 function oldestLivingFather(){
-people.forEach(function(person){
-	var personName = person['name']
-	var personAge = person['age']
-	var fatherArray = getFathersNames()
-if (fatherArray.indexOf(personName) > -1 && person['age'] > highestAge) {
-	 {
+		var fatherArray = function () {
+		var fathers = [];
+		for (var i = 0; i < people.length; i++) {
+			if (people[i].father){
+			var fatherName = people[i].father;
+				if (fathers.indexOf(fatherName) === -1) {
+				fathers.push(fatherName);
+					}
+				}
+			}
+		return fathers;
+		};
+	people.forEach(function(person){
+	var personName = person['name'];
+	var personAge = person['age'];
+	if (fatherArray().indexOf(personName) > -1 && person['age'] > highestAge) {
 				highestAge = personAge;
 				oldestFather = personName;
-			}
 	}
 
-});
-return oldestFather + ' is oldest father and he is ' + highestAge
+	});
+return oldestFather + ' is oldest father and he is ' + highestAge;
 }
 
 
