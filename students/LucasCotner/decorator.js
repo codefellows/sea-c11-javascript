24429dfe7694d265b6791f248c4dc99031dfdb35
SwampGuzzler <lc07@uw.edu>
2014-04-21 20:07:37 -0700

    Update Decorator

--- students/LucasCotner/decorator.js
+++ students/LucasCotner/decorator.js
@@ -1,13 +1,18 @@
-var countDecorator = function(item) {
+var countDecorator = function(func) {
     var count = 0;
-    console.log(count); 
-    function item() {
-        count++;
-        console.log(count);
+    console.log("counter in the outside function: " + count); 
+    function change(val) {
+        //count++;
+        count = count + val;
+        console.log("counter in the closure: " + count);
     }
+    
+    return change(2);
+    
 }
+//console.log(countDecorator());
+var first = countDecorator();
+//console.log(first);
+//item();
+console.log('');
 
-countDecorator();
-item();
-console.log('');
-console.log('');
