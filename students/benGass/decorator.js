a45734c01e5051bbf2659c003988c29da7bedd2b
bgas <benjamin12g@gmail.com>
2014-04-22 13:57:57 -0700

    test cases and console logs removed

--- students/benGass/decorator.js
+++ students/benGass/decorator.js
@@ -11,13 +11,7 @@ var countDecorator = function (func) {
   return wrapper;
 };
 
-
 function add(x, y) {  return x + y; }
 
 //had to modify
 var add = countDecorator(add);
-
-add(2, 3);
-add(2, 5);
-add(2, 1);
-console.log(add.getCount());
