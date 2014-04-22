c6ceb1a6710a9e63478e06fbe2772cdea7d6b25d
Adam Brown <brownad78@gmail.com>
2014-04-21 18:55:10 -0700

    Clean up extra code

--- students/adambrown/oldestLivingFather.js
+++ students/adambrown/oldestLivingFather.js
@@ -1,38 +1,3 @@
-// Given the following data structure
-// implement a oldestLivingFather method 
-// that will return the name of the oldest 
-// living father.
-
-var people = [
-  {
-    name: 'Hank',
-    age: 29,
-    father: 'Don'
-  },
-  {
-    name: 'Deborah',
-    age: 30,
-    father: 'Don'
-  },
-  {
-    name: 'Danny',
-    age: 24,
-    father: 'Don'
-  },
-  {
-    name: 'Don',
-    age: 64,
-    father: 'Haskell'
-  },
-  {
-    name: 'Haskell',
-    father: 'Finus'
-  },
-  {
-    name: 'Finus'
-  }
-];
-
 function oldestLivingFather(people){
   var oldest = 0;
   var dadNames=[];
