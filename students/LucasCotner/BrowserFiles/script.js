ce49eeaa944ef8a98e7c2ae7ffc35efd667d6e05
SwampGuzzler <lc07@uw.edu>
2014-04-23 14:20:57 -0700

    Tabs Assignment

--- students/LucasCotner/BrowserFiles/script.js
+++ students/LucasCotner/BrowserFiles/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-    $('a').on('click', function(e) {
-      $(e.target).toggleClass('red');
-    });
     $('.tab').mouseenter(function() {
         $(this).css("background-color","yellow");
+        $(this).css("outline","#00FF00 solid 2px");
     });
     $('.tab').mouseleave(function() {
         $('.tab').css("background-color","green");
+        $(this).css("outline","none");
     });
-    $('a').on('click', function(e) {
-        $('.tabs2').toggle();
+    $('#home2').on('click', function(e) {
+        $('#home').css("display","block");
+        $('#articles').css("display","none");
+        $('#portfolio').css("display","none");
+    });
+    $('#articles2').on('click', function(e) {
+        $('#articles').css("display","block");
+        $('#home').css("display","none");
+        $('#portfolio').css("display","none");
+    });
+    $('#portfolio2').on('click', function(e) {
+        $('#portfolio').css("display","block");
+        $('#home').css("display","none");
+        $('#articles').css("display","none");
     });
 });
