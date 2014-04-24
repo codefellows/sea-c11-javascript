7c57cc053a4842046c61d6e9e394c92a55fa9384
Hailey Petway <petwayh@gmail.com>
2014-04-23 17:02:59 -0700

    Updating all tabs files

--- students/hlpetway/tabs/tabs.js
+++ students/hlpetway/tabs/tabs.js
@@ -1,20 +1,18 @@
-$(document).ready(function() {
-$('.nav-tabs > li > a').click(function (event) {
-    event.preventDefault();
+$(document).ready(function () {
+    $('a').click(function (event) {
+        event.preventDefault();
 
-    var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');
-
-    var actived_nav = $('.nav-tabs > li.active');
-    actived_nav.removeClass('active');
-
-    $(this).parents('li').addClass('active');
-
-    $(active_tab_selector).removeClass('active');
-    $(active_tab_selector).addClass('hide');
-
-    var target_tab_selector = $(this).attr('href');
-    $(target_tab_selector).removeClass('hide');
-    $(target_tab_selector).addClass('active');
-
-});
-});
+        $(function () {
+            $('a').on('click', function (e) {
+                e.preventDefault();
+                $('.tab-content').removeClass('active');
+                var target = $(this).attr('href');
+                $(target).addClass('active');
+                
+                $('.nav').removeClass('active');
+                var targetNav = $(this).attr('href');
+                $(targetNav).addClass('active');
+            });
+        });
+            
+    });
