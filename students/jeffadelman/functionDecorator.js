ac60e5a1696649ca8af26313dceb8b82230b3822
Jeff Adelman <jeffadelman8@yahoo.com>
2014-04-24 00:47:35 -0700

    Correct version

--- students/jeffadelman/functionDecorator.js
+++ students/jeffadelman/functionDecorator.js
@@ -1,18 +1,12 @@
 
 function countDecorator (func){
   var count = 0; 
-  var b = 0;
   function wrapper(){
     count++;
     return func.apply(this, arguments);
   }
   wrapper.callCount = function(){
-    var psw = prompt('Please enter your password');
-    function(psw){
-      if(psw===1234){
-        console.log(count);
-        return count;
-      };
+      return count;
   };
   return wrapper;
 }
