143e343f046a938abca37037e483e8189572811c
Paul Laskowski <pelaskowski@gmail.com>
2014-04-23 19:22:51 -0700

    removed errent console.log calls from debugging

--- students/paul-laskowski/tabs/tabs.js
+++ students/paul-laskowski/tabs/tabs.js
@@ -2,12 +2,10 @@
 $( document ).ready(function() {
 	$('div.tabcontent').hide();
 	$('section.taboutput').html($('div#home').html());
-	console.log('hellooooo');
 
 	$('a').on('click', function (e) {
 	    clicked = (e.target).href.split('#');
 	    clickedClass = 'div#' + clicked[1];
-		console.log(clickedClass);
 	    $('section.taboutput').hide();
 	    $('section.taboutput').html($(clickedClass).html());
 	    $('section.taboutput').fadeIn();
