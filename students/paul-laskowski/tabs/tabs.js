46530035b7376c5d0eab682ca5888a5f8c14fa9f
Paul Laskowski <pelaskowski@gmail.com>
2014-04-23 19:21:15 -0700

    moved code into body of full html document and referenced scripts and css document, just load /tabs.html

--- students/paul-laskowski/tabs/tabs.js
+++ students/paul-laskowski/tabs/tabs.js
@@ -1,9 +1,15 @@
-$('div.tabcontent').hide();
-$('section.taboutput').html($('div#home').html());
-$('a').on('click', function (e) {
-    clicked = (e.target).href.split('#');
-    clickedClass = 'div#' + clicked[1];
-    $('section.taboutput').hide();
-    $('section.taboutput').html($(clickedClass).html());
-    $('section.taboutput').fadeIn();
+
+$( document ).ready(function() {
+	$('div.tabcontent').hide();
+	$('section.taboutput').html($('div#home').html());
+	console.log('hellooooo');
+
+	$('a').on('click', function (e) {
+	    clicked = (e.target).href.split('#');
+	    clickedClass = 'div#' + clicked[1];
+		console.log(clickedClass);
+	    $('section.taboutput').hide();
+	    $('section.taboutput').html($(clickedClass).html());
+	    $('section.taboutput').fadeIn();
+	});
 });
