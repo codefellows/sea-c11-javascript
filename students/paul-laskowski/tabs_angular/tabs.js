4652fde841526e01b9fd4da3d3dcd03a273c2761
Paul Laskowski <pelaskowski@gmail.com>
2014-05-01 22:55:30 -0700

    Added custom capitalization filter module and bootstrapped myApp with dependency on filters module

--- students/paul-laskowski/tabs_angular/tabs.js
+++ students/paul-laskowski/tabs_angular/tabs.js
@@ -1,23 +1,23 @@
+angular.module('myApp', ['filters'])
 
-function tabController($scope, $http) {
+	.controller('tabController', function($scope, $http) {
 	
-	$scope.tabs = [];
-	$http.get('http://rs.hankyates.com:3000/v2/content').success(gotData);
-	
-	$scope.showTab = function(tab){
-		$scope.activeTab = tab;
-	};
-	
-  function gotData(tabs){
-	  $scope.tabs = tabs;
-	  $scope.activeTab = $scope.tabs[0];
-  }
+		$scope.tabs = [];
+		$http.get('http://rs.hankyates.com:3000/v2/content').success(gotData);
+		
+		$scope.showTab = function(tab){
+			$scope.activeTab = tab;
+		};
+		
+		function gotData(tabs){
+			$scope.tabs = tabs;
+			$scope.activeTab = $scope.tabs[0];
+		}
+	});
 
-};
-
-// angular.module('ngFilters', []).filter('capitalize', function () {
-//     "use strict";
-//     return function (input) {
-//         return input.charAt(0).toUpperCase() + input.slice(1);
-//     };
-// });
+angular.module('filters',[])
+	.filter('capitalize', function () {
+		return function (input) {
+			return input.charAt(0).toUpperCase() + input.slice(1);
+		};
+	});
