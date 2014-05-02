edfb46be9134351ab0b79f8a2923ffaa2827b09c
Zach Bryan <zachx1@gmail.com>
2014-05-02 12:45:50 -0700

    resolved issues within tabs

--- students/ZachBryan/tabs/js/main.js
+++ students/ZachBryan/tabs/js/main.js
@@ -1,10 +1,10 @@
-$('.tab').on('mouseover',function () {
-  $('#holder > .tabs > li.active').removeClass('active');
+$('a').on('click',function () {
+  $('li').removeClass('active');
 
   $(this).parent().addClass('active');
 
-  $('#holder > .tab_container > div.active_tab').removeClass('active_tab');
+  $('section div').removeClass('show');
 
-  $(this.rel).addClass('active_tab');
+  $(this.rel).addClass('show');
 
  });
